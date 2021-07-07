8574f4a8104fb1ea014b21fcb2e0b31f4c0b6236
Furukawa <ancient@EishinnoMacBook-Air.local>
2021-07-08 00:20:05 +0900

    Greeting added

--- saikoro.java
+++ saikoro.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 import java.util.Random;
 public class saikoro {
 public static void main(String[] args) {
     Random rand = new Random();
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("what is your name?");
+String name=scanner.next();
+System.out.println("Hello, "+name+"!");
 System.out.println("Rolling the dice");
 int val1=rand.nextInt(6)+1;
 System.out.println("Die1:" + val1);
